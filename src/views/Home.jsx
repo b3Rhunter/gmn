b9a1286e7d390ff08c009baab50a474cdf3efb68
Pub <96320073+b3Rhunter@users.noreply.github.com>
2022-06-18 02:42:12 -0500

    Add files via upload

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,6 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState } from "react";
 import { Button, notification } from "antd";
 import { ethers } from "ethers";
-import { Transactor } from "../helpers";
-import { MinusOutlined } from "@ant-design/icons";
-
-
-
-
 
 function Home({ 
   userSigner, 
@@ -24,15 +18,17 @@ function Home({
   const [isSigning, setIsSigning] = useState(false);
   const [isAuth, setIsAuth] = useState(false);
 
+  // Mint Daily 🗞️
+
   const signer = (new ethers.providers.Web3Provider(window.ethereum)).getSigner();
   const contract = new ethers.Contract("0xfD18418c4AEf8edcAfF3EFea4A4bE2cC1cF2E580", [{"inputs":[{"internalType":"string","name":"_name","type":"string"},{"internalType":"string","name":"_symbol","type":"string"},{"internalType":"string","name":"_initBaseURI","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"cost","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxMintAmount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_mintAmount","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bool","name":"_state","type":"bool"}],"name":"pause","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"paused","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_newBaseURI","type":"string"}],"name":"setBaseURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_newCost","type":"uint256"}],"name":"setCost","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_newmaxMintAmount","type":"uint256"}],"name":"setmaxMintAmount","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_owner","type":"address"}],"name":"walletOfOwner","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"payable","type":"function"}], signer);
-  const gas_limit = "350000"
-  const cost = contract.cost()
+  const cost = contract.cost();
 
   function Mint() {
     contract.mint(1, {value: cost})
   };
 
+// 🗞️🗞️🗞️🗞️🗞️🗞️🗞️🗞️🗞️🗞️
 
 
   const sendNotification = (type, data) => {
@@ -42,8 +38,7 @@ function Home({
     });
   };
 
-
-
+// Token Gate 🚫
 
   const validateUser = async (message, address, signature) => {
     // validate signature
@@ -71,6 +66,8 @@ function Home({
       return false;
     }
   };
+
+// Sign In With Ethereum
 
   const handleSignIn = async () => {
     if (web3Modal.cachedProvider === "") {
@@ -110,34 +107,41 @@ function Home({
     setIsSigning(false);
   };
 
+// Display 👇 
+
   return (
+
 <div style={{height: "100%"}}>
-<div>
-<Button shape="round" size="large" style={{ marginTop: 32, marginLeft: 10, marginRight: 10 }} await onClick={Mint}>
-Mint
-</Button>
-<Button shape="round" size="large" loading={isSigning} style={{ marginTop: 32, marginLeft: 10, marginRight: 10  }} await onClick={handleSignIn}>
-        Verify
-</Button>
-    </div>
 
-      {isAuth && (
+  <div>
 
-        <div style={{ marginTop: 20, width: "100%", height: "100%" }}>
-          <iframe
-            style={{ width: "90%", height: "960px", borderRadius: "2%" }}
-            allowfullscreen=""
-            mozallowfullscreen="true"
-            oallowfullscreen="true"
-            msallowfullscreen="true"
-            title="GMN"
-            src="https://interactive-umber.vercel.app/"
-          ></iframe>
-        </div>
-      )}
-    </div>
-  );
-}
+    <Button shape="round" size="large" style={{ marginTop: 32, marginLeft: 10, marginRight: 10 }} await onClick={Mint}>
+      Mint
+    </Button>
 
+    <Button shape="round" size="large" loading={isSigning} style={{ marginTop: 32, marginLeft: 10, marginRight: 10  }} await onClick={handleSignIn}>
+      Verify
+    </Button>
+
+  </div>
+
+  {isAuth && (
+
+  // Gated Content
+
+  <div style={{ marginTop: 20, width: "100%", height: "100%" }}>
+    <iframe
+      style={{ width: "90%", height: "960px", borderRadius: "2%" }}
+      allowfullscreen=""
+      mozallowfullscreen="true"
+      oallowfullscreen="true"
+      msallowfullscreen="true"
+      title="GMN"
+      src="https://interactive-umber.vercel.app/">
+    </iframe>
+  </div>
+  )}
+
+</div>);}
 
 export default Home;
