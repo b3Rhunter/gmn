ab5f0a55c12e4df884d4f88a09583468d6b9d888
Pub <96320073+b3Rhunter@users.noreply.github.com>
2022-06-18 02:43:00 -0500

    Add files via upload

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,54 +1,12 @@
 import { Button } from "antd";
 import React from "react";
-import Address from "./Address";
-import Balance from "./Balance";
-import Wallet from "./Wallet";
 
-/*
-  ~ What it does? ~
 
-  Displays an Address, Balance, and Wallet as one Account component,
-  also allows users to log in to existing accounts and log out
-
-  ~ How can I use? ~
-
-  <Account
-    address={address}
-    localProvider={localProvider}
-    userProvider={userProvider}
-    mainnetProvider={mainnetProvider}
-    price={price}
-    web3Modal={web3Modal}
-    loadWeb3Modal={loadWeb3Modal}
-    logoutOfWeb3Modal={logoutOfWeb3Modal}
-    blockExplorer={blockExplorer}
-  />
-
-  ~ Features ~
-
-  - Provide address={address} and get balance corresponding to the given address
-  - Provide localProvider={localProvider} to access balance on local network
-  - Provide userProvider={userProvider} to display a wallet
-  - Provide mainnetProvider={mainnetProvider} and your address will be replaced by ENS name
-              (ex. "0xa870" => "user.eth")
-  - Provide price={price} of ether and get your balance converted to dollars
-  - Provide web3Modal={web3Modal}, loadWeb3Modal={loadWeb3Modal}, logoutOfWeb3Modal={logoutOfWeb3Modal}
-              to be able to log in/log out to/from existing accounts
-  - Provide blockExplorer={blockExplorer}, click on address and get the link
-              (ex. by default "https://etherscan.io/" or for xdai "https://blockscout.com/poa/xdai/")
-*/
 
 export default function Account({
-  address,
-  userSigner,
-  localProvider,
-  mainnetProvider,
-  price,
-  minimized,
   web3Modal,
   loadWeb3Modal,
   logoutOfWeb3Modal,
-  blockExplorer,
 }) {
   const modalButtons = [];
   if (web3Modal) {
@@ -56,7 +14,6 @@ export default function Account({
       modalButtons.push(
         <Button
           key="logoutbutton"
-          style={{ verticalAlign: "top", marginBottom: 10 }}
           shape="round"
           size="large"
           onClick={logoutOfWeb3Modal}
@@ -68,7 +25,6 @@ export default function Account({
       modalButtons.push(
         <Button
           key="loginbutton"
-          style={{ verticalAlign: "top", marginBottom: 10 }}
           shape="round"
           size="large"
           /* type={minimized ? "default" : "primary"}     too many people just defaulting to MM and having a bad time */
@@ -80,5 +36,5 @@ export default function Account({
     }
   }
 
-  return <div>{modalButtons}</div>;
+  return <div style={{position: "absolute", top:33, left: 33, right: 33}}>{modalButtons}</div>;
 }
